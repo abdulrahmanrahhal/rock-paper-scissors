1b6dbd62560e013fbd3ba78feec720a87f47e773
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-01-30 14:28:53 +0300

    Add Score to the result pane

--- script.js
+++ script.js
@@ -10,16 +10,31 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     if(playerSelection === computerSelection)
-        resultDiv.textContent =  `It's a tie! ${playerSelection} and ${computerSelection}`;
-    else if((computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper"))
-        resultDiv.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
-    else
-        resultDiv.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        resultDiv.innerHTML =  `<br>It's a tie! ${playerSelection} and ${computerSelection}<br> Score: ${playerScore}:${computerScore}`;
+    else if((computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper")){
+        computerScore++;
+        resultDiv.innerHTML = `<br>You Lose! ${computerSelection} beats ${playerSelection}<br> Score: ${playerScore}:${computerScore}`;
+    }
+    else{
+        playerScore++;
+        resultDiv.innerHTML = `<br>You Win! ${playerSelection} beats ${computerSelection}<br> Score: ${playerScore}:${computerScore}`;
+    }
 }
 const resultDiv = document.querySelector('div');
 const buttons = document.querySelectorAll('button');
+
+let playerScore = 0;
+let computerScore = 0;
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         playRound(button.textContent, getComputerChoice())
+        if(playerScore === 5 || computerScore === 5){
+            if(playerScore === 5)
+            resultDiv.innerHTML = `<br>Congratulations🎉.. You Win`;
+            else
+            resultDiv.innerHTML = `<br>You Lose`;
+            playerScore = 0;
+            computerScore = 0;
+        }
     })
 })
