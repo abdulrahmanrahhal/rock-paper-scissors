70509902d24d398f0e5353472e8e000ef42dbcff
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-01-30 13:37:58 +0300

    add buttons and result on page instead of prompt

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ function getComputerChoice(){
         return "paper";
     else
         return "scissors";
-}
+    }
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection);
     if(playerSelection === computerSelection)
-        return `It's a tie! ${playerSelection} and ${computerSelection}`;
+        resultDiv.textContent =  `It's a tie! ${playerSelection} and ${computerSelection}`;
     else if((computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper"))
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
+        resultDiv.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
     else
-        return `You Win! ${playerSelection} beats ${computerSelection}`
+        resultDiv.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
 }
-for (let i = 0; i < 5; i++) {
-    const  playerSelection = prompt("Choice Rock, Paper or Scissors");
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    // your code here!
-}
+const resultDiv = document.querySelector('div');
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.textContent, getComputerChoice())
+    })
+})
