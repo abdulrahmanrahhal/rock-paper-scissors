4729406abd773c8089bd0279089f76900cdf2af9
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-03-08 08:56:23 +0300

    Make script more readable

--- script.js
+++ script.js
@@ -1,49 +1,106 @@
-function getComputerChoice(){
-    let randomNum = Math.floor((Math.random() * 3));
-    if(randomNum === 0)
-        return "rock";
-    else if(randomNum === 1)
-        return "paper";
-    else
-        return "scissors";
-    }
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    if(playerSelection === computerSelection){
-        score.innerHTML = `${playerScore} : ${computerScore}`
-        resultDiv.innerHTML +=  `<span style="color: var(--yellow); display: inline;">It's a tie! ${playerSelection} and ${computerSelection}</span>`;
-}
-    else if((computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper")){
-        computerScore++;
-        score.innerHTML = `${playerScore} : ${computerScore}`
-        resultDiv.innerHTML += `<span style="color: var(--red); display: inline;">You Lose! ${computerSelection} beats ${playerSelection}</span>`;
-    }
-    else{
-        playerScore++;
-        score.innerHTML = `${playerScore} : ${computerScore}`
-        resultDiv.innerHTML += `<span style="color: var(--green); display: inline;">You Win! ${playerSelection} beats ${computerSelection}</span> `;
-    }
-}
-const resultDiv = document.querySelector('.result-pane');
-const buttons = document.querySelectorAll('button');
-const score = document.querySelector('.score-div');
+const gameController = (() => {
+  let playerScore = 0;
+  let computerScore = 0;
+  const getComputerChoice = () => {
+    const randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0) return 'rock';
+    else if (randomNum === 1) return 'paper';
+    else return 'scissors';
+  };
+  const getComputerScore = () => computerScore;
+  const getPlayerScore = () => playerScore;
 
-let playerScore = 0;
-let computerScore = 0;
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        playRound(button.value, getComputerChoice())
-        if(playerScore === 5 || computerScore === 5){
-            if(playerScore === 5){
-            playerScore = 0;
-            computerScore = 0;
-            resultDiv.innerHTML = `<br>Congratulations🎉.. You Win`;
-            }
-            else{
-            computerScore = 0;
-            playerScore = 0;
-            resultDiv.innerHTML = `<br>You Lose`;
-            }
-        }
+  const checkWinner = () => {
+    if (playerScore === 5){
+      displayController.displayWinner('YOU');
+      playerScore = 0;
+      computerScore = 0;
+      return;
+    } 
+    else if (computerScore === 5){
+      displayController.displayWinner('COMPUTER');
+      playerScore = 0;
+      computerScore = 0;
+      return;
+    }
+  };
+  const playRound = (player) => {
+    const computer = getComputerChoice();
+    if (
+      (computer === 'rock' && player === 'paper') ||
+      (computer === 'paper' && player === 'scissors') ||
+      (computer === 'scissors' && player === 'rock')
+    ) {
+      playerScore++;
+      displayController.logWin(player, computer);
+    } else if (
+      (computer === 'rock' && player === 'rock') ||
+      (computer === 'paper' && player === 'paper') ||
+      (computer === 'scissors' && player === 'scissors')
+    ) {
+      displayController.logTie(player, computer);
+    } else {
+      computerScore++;
+      displayController.logLose(player, computer);
+    }
+    checkWinner();
+  };
+  return {
+    getPlayerScore,
+    getComputerScore,
+    playRound,
+  };
+})();
+
+const displayController = (() => {
+  const buttons = document.querySelectorAll('.btn');
+  const resultsList = document.querySelector('.results-list');
+  const resultWrapper = document.querySelector('.result-wrapper');
+  const score = document.querySelector('.score');
+  const winnerPanel = document.querySelector('.winner');
+
+  const cleanResults = () => {
+    resultsList.textContent = '';
+  };
+  const changeBorder = (color) => {
+    resultWrapper.style.borderColor = `var(--clr-${color})`;
+  };
+  const logLose = (player, computer) => {
+    changeBorder('red');
+    resultsList.innerHTML += `<p>You Lose: You chose ${player} and computer chose ${computer}</p>`;
+  };
+  const logWin = (player, computer) => {
+    changeBorder('green');
+    resultsList.innerHTML += `<p>You Win: You chose ${player} and computer chose ${computer}</p>`;
+  };
+  const logTie = (player, computer) => {
+    changeBorder('yellow');
+    resultsList.innerHTML += `<p>It is a tie: You chose ${player} and computer chose ${computer}</p>`;
+  };
+  const updateResult = () => {
+    score.textContent = `${gameController.getPlayerScore()} : ${gameController.getComputerScore()}`;
+  };
+  const displayWinner = (winnerName) => {
+    cleanResults();
+    winnerPanel.innerHTML = `<p>${winnerName} ARE THE WINNER.</p>`
+    winnerPanel.classList.add('show');
+    setTimeout(() => {
+      winnerPanel.classList.remove('show');
+    }, 2000);
+    winnerPanel.textContent = '';
+  };
+  buttons.forEach((btn) =>
+    btn.addEventListener('click', (e) => {
+      const player = e.target.parentElement.value;
+      gameController.playRound(player);
+      updateResult();
     })
-})
+  );
+  return {
+    updateResult,
+    logWin,
+    logLose,
+    logTie,
+    displayWinner,
+  };
+})();
